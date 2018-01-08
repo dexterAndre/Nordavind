5ae99e4bb7534cc6708b99c79f2a83344019cb3c
Dexter André Osiander <dexter.andre@outlook.com>
2018-01-08 15:54:27 +0100

    Added variables
    Done nothing new with the script though. Stilll need to add more things.

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,14 +4,13 @@ using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
-	// Input
-	private Vector2 inputL;
-	private Vector2 inputR;
-	private float inputTriggers;
+	[Header("Input")]
 	[SerializeField]
-	private float triggersSensitivity;
+	private float mTriggersSensitivity = 1f;
+	private Vector2 mInputL;
+	private Vector2 mInputR;
+	private float mInputTriggers;
 
-	// Player State
 	[System.Serializable]
 	public enum PlayerState
 	{
@@ -22,63 +21,104 @@ public class PlayerController : MonoBehaviour
 		Throw
 	}
 
-	// Movement
+	[Header("Player State")]
 	[SerializeField]
-	private CharacterController characterController;
-	[SerializeField]
-	private float movementSpeed;
-	private Vector3 movementVector;
+	private PlayerState mState = PlayerState.Walk;
 
-	// Camera
+	[Header("Movement")]
 	[SerializeField]
-	private CinemachineFreeLook freeLook;
-	private Quaternion cameraDirection;
-	private float[] rigRadii = new float[3];
-	private float cameraDistance = 1f;
+	private float mWalkSpeed = 5f;
+	private CharacterController mCharacterController = null;
+	private Vector3 mMovementVector;
+	private Vector3 mMovementVelocity;
+	private Vector3 mMovementGravity = new Vector3(0f, -9.81f, 0f);
+
+	[Header("Rotation")]
+	[SerializeField]
+	private float mRotationSpeed = 1f;
+
+	[Header("Jumping")]
+	[SerializeField]
+	private float mJumpForce = 1f;
+	private bool mIsGrounded = true;
+	private RaycastHit mGroundCheck;
+	[SerializeField]
+	private float mGroundCheckOffset = 0.1f;
+	[SerializeField]
+	private float mGroundCheckRadius = 0.5f;
+	private Vector3 mGroundNormalCurrent;
+
+	[Header("Dash / Roll")]
+	[SerializeField]
+	private float mDashSpeed = 10f;
+	[SerializeField]
+	private float mDashCooldown = 2f;
+	private float mDashCooldownTimer = 0f;
+	[SerializeField]
+	private float mDashDuration = 0.05f;
+
+	[Header("Hanging")]
+	[SerializeField]
+	private float mHangHeightHigh = 2f;
+	[SerializeField]
+	private float mHangHeightLow = 1f;
+	[SerializeField]
+	private float mHangHeightDelta = 0.25f;
+	
+
+	[Header("Camera")]
+	[SerializeField]
+	private float mCameraDistance = 1f;
+	[SerializeField]
+	private CinemachineFreeLook mFreeLook = null;
+	private Quaternion mCameraDirection;
+	private float[] mRigRadii = new float[3];
 
 
 
 	private void Start ()
 	{
-		characterController = GetComponent<CharacterController>();
+		mCharacterController = GetComponent<CharacterController>();
+
+		// Storing initial camera distances for all 3 camera rigs. 
 		for (int i = 0; i < 3; i++)
 		{
-			rigRadii[i] = freeLook.m_Orbits[i].m_Radius;
+			mRigRadii[i] = mFreeLook.m_Orbits[i].m_Radius;
 		}
 	}
 
 	private void Update ()
 	{
 		// Input
-		inputL = new Vector2
+		mInputL = new Vector2
 		(
 			Input.GetAxisRaw("Horizontal"),
 			Input.GetAxisRaw("Vertical")
 		);
-		inputR = new Vector2
+		mInputR = new Vector2
 		(
 			Input.GetAxisRaw("RHorizontal"),
 			Input.GetAxisRaw("RVertical")
 		);
-		inputTriggers = Input.GetAxisRaw("Triggers");
-		cameraDirection = Camera.main.transform.rotation;
+		mInputTriggers = Input.GetAxisRaw("Triggers");
+		mCameraDirection = Camera.main.transform.rotation;
 
 		// Movement
-		movementVector = new Vector3(inputL.x, movementVector.y, inputL.y);
-		movementVector = Vector3.ProjectOnPlane((cameraDirection * movementVector), Vector3.up).normalized * movementSpeed * Time.deltaTime;
+		mMovementVector = new Vector3(mInputL.x, mMovementVector.y, mInputL.y);
+		mMovementVector = Vector3.ProjectOnPlane((mCameraDirection * mMovementVector), Vector3.up).normalized * mWalkSpeed * Time.deltaTime;
 
 		// Camera controls
 		// [The right stick is configured within Cinemachine]
-		cameraDistance += inputTriggers * triggersSensitivity;
-		if (cameraDistance < 0.0f)
-			cameraDistance = 0.0f;
+		mCameraDistance += mInputTriggers * mTriggersSensitivity;
+		if (mCameraDistance < 0.0f)
+			mCameraDistance = 0.0f;
 
 		for (int i = 0; i < 3; i++)
 		{
-			freeLook.m_Orbits[i].m_Radius = rigRadii[i] * cameraDistance;
+			mFreeLook.m_Orbits[i].m_Radius = mRigRadii[i] * mCameraDistance;
 		}
 
-		characterController.Move(movementVector);
+		mCharacterController.Move(mMovementVector);
 	}
 
 	private void FixedUpdate ()
