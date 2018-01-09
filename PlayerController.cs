a3a03bc27904479cab87a3cb765d9fa96fb68d7c
Dexter André Osiander <dexter.andre@outlook.com>
2018-01-09 11:57:35 +0100

    Better movement
    Added rotation, added commented-out variables for other mechanics as well

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,68 +4,103 @@ using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
+/* 
+	#########################
+	######### TO-DO #########
+	#########################
+	- Mouse and keyboard support
+	- Auto-detect when changing from gamepad to mouse and keyboard
+	- Get rid of mIsGrounded
+*/
+
+#region Input
+	// Temporary controls using a gamepad
 	[Header("Input")]
+	// [SerializeField]
+	// private float mTriggersSensitivity = 1f;
+	private float mInputTriggerL;
+	// private float mInputTriggerR;
 	[SerializeField]
-	private float mTriggersSensitivity = 1f;
-	private Vector2 mInputL;
-	private Vector2 mInputR;
-	private float mInputTriggers;
+	private float mDPadSensitivity = 1f;
+	// private float mInputDPadX;
+	private float mInputDPadY;
+	private Vector2 mInputLStick;
+	// private Vector2 mInputRStick;
+#endregion
 
-	[System.Serializable]
-	public enum PlayerState
-	{
-		Walk,
-		Fall,
-		Hang,
-		Climb,
-		Throw
-	}
+#region Player State
+	// [System.Serializable]
+	// public enum PlayerState
+	// {
+	// 	Walk,
+	// 	Fall,
+	// 	Hang,
+	// 	Climb,
+	// 	Throw
+	// }
+	// [Header("Player State")]
+	// [SerializeField]
+	// private PlayerState mState = PlayerState.Walk;
+#endregion
 
-	[Header("Player State")]
-	[SerializeField]
-	private PlayerState mState = PlayerState.Walk;
-
+#region Movement
 	[Header("Movement")]
 	[SerializeField]
 	private float mWalkSpeed = 5f;
 	private CharacterController mCharacterController = null;
 	private Vector3 mMovementVector;
 	private Vector3 mMovementVelocity;
-	private Vector3 mMovementGravity = new Vector3(0f, -9.81f, 0f);
+#endregion
 
+#region Rotation
 	[Header("Rotation")]
 	[SerializeField]
-	private float mRotationSpeed = 1f;
+	private float mRotationSlerpParameter = 0.3f;
+#endregion
 
-	[Header("Jumping")]
-	[SerializeField]
-	private float mJumpForce = 1f;
-	private bool mIsGrounded = true;
-	private RaycastHit mGroundCheck;
-	[SerializeField]
-	private float mGroundCheckOffset = 0.1f;
-	[SerializeField]
-	private float mGroundCheckRadius = 0.5f;
-	private Vector3 mGroundNormalCurrent;
+#region Jumping
+	// [Header("Jumping")]
+	// [SerializeField]
+	// private float mJumpForce = 1f;
+	// private float mJumpTimer = 0f;
+	// private float mVerticalMovement = 0f;
+	// [SerializeField]
+	// private bool mIsGrounded = true;
+	// private RaycastHit mGroundCheck;
+	// [SerializeField]
+	// private float mGroundCheckOffset = 0.1f;
+	// [SerializeField]
+	// private float mGroundCheckRadius = 0.5f;
+	// private Vector3 mGroundNormalCurrent;
+	// private float mGravity = -9.81f;
+	// [SerializeField]
+	// private float mGravityScale = 1f;
+#endregion
 
-	[Header("Dash / Roll")]
-	[SerializeField]
-	private float mDashSpeed = 10f;
-	[SerializeField]
-	private float mDashCooldown = 2f;
-	private float mDashCooldownTimer = 0f;
-	[SerializeField]
-	private float mDashDuration = 0.05f;
+#region Dash
+	// [Header("Dash / Roll")]
+	// [SerializeField]
+	// private float mDashSpeed = 10f;
+	// [SerializeField]
+	// private float mDashCooldown = 2f;
+	// private float mDashCooldownTimer = 0f;
+	// [SerializeField]
+	// private float mDashDuration = 0.05f;
+#endregion
 
-	[Header("Hanging")]
-	[SerializeField]
-	private float mHangHeightHigh = 2f;
-	[SerializeField]
-	private float mHangHeightLow = 1f;
-	[SerializeField]
-	private float mHangHeightDelta = 0.25f;
+#region Hanging
+	// [Header("Hanging")]
+	// [SerializeField]
+	// private float mHangHeightHigh = 2f;
+	// [SerializeField]
+	// private float mHangHeightLow = 1f;
+	// [SerializeField]
+	// private float mHangHeightDelta = 0.25f;
+	// [SerializeField]
+	// private float mClimbAnimationDuration = 0.25f;
+#endregion
 	
-
+#region Camera
 	[Header("Camera")]
 	[SerializeField]
 	private float mCameraDistance = 1f;
@@ -73,9 +108,29 @@ public class PlayerController : MonoBehaviour
 	private CinemachineFreeLook mFreeLook = null;
 	private Quaternion mCameraDirection;
 	private float[] mRigRadii = new float[3];
+#endregion
 
+#region Debug
+	// [Header("Debug")]
+	// [SerializeField]
+	// private bool mIsDebugging = true;
+	// [SerializeField]
+	// private Vector3 mDebugOffset = new Vector3(0f, 2f, 0f);
+	// private Vector3 mDebugHangPoint;
+	// [SerializeField]
+	// private Color mDebugColorHangTop, mDebugColorHangPoint, mDebugColorHandPlanar, mDebugColorHangDirection;
+	
+	/// <summary> 
+	/// Runs the debug code. 
+	/// </summary>
+	/// <param name="other"></param>
+	private void VisualDebug()
+	{
 
+	}
+#endregion
 
+#region Update Functions
 	private void Start ()
 	{
 		mCharacterController = GetComponent<CharacterController>();
@@ -89,39 +144,56 @@ public class PlayerController : MonoBehaviour
 
 	private void Update ()
 	{
-		// Input
-		mInputL = new Vector2
+	#region Input
+		mInputLStick = new Vector2
 		(
 			Input.GetAxisRaw("Horizontal"),
 			Input.GetAxisRaw("Vertical")
 		);
-		mInputR = new Vector2
-		(
-			Input.GetAxisRaw("RHorizontal"),
-			Input.GetAxisRaw("RVertical")
-		);
-		mInputTriggers = Input.GetAxisRaw("Triggers");
+		// mInputRStick = new Vector2
+		// (
+		// 	Input.GetAxisRaw("RHorizontal"),
+		// 	Input.GetAxisRaw("RVertical")
+		// );
+		// mInputDPadX = Input.GetAxisRaw("DPadX");
+		mInputDPadY = Input.GetAxisRaw("DPadY");
+	#endregion
+
+	#region Camera Controls
 		mCameraDirection = Camera.main.transform.rotation;
 
-		// Movement
-		mMovementVector = new Vector3(mInputL.x, mMovementVector.y, mInputL.y);
-		mMovementVector = Vector3.ProjectOnPlane((mCameraDirection * mMovementVector), Vector3.up).normalized * mWalkSpeed * Time.deltaTime;
-
-		// Camera controls
-		// [The right stick is configured within Cinemachine]
-		mCameraDistance += mInputTriggers * mTriggersSensitivity;
+		// Camera distance (for testing only)
+		mCameraDistance += mInputDPadY * mDPadSensitivity;
 		if (mCameraDistance < 0.0f)
 			mCameraDistance = 0.0f;
-
-		for (int i = 0; i < 3; i++)
+		// Setting camera dolly distances for all 3 rigs. 
+		if (mInputDPadY != 0.0f)
 		{
-			mFreeLook.m_Orbits[i].m_Radius = mRigRadii[i] * mCameraDistance;
+			for (int i = 0; i < 3; i++)
+			{
+				mFreeLook.m_Orbits[i].m_Radius = mRigRadii[i] * mCameraDistance;
+			}
 		}
+	#endregion
 
+	#region Movement
+		// Calculating movement vector
+		mMovementVector = new Vector3(mInputLStick.x, 0f, mInputLStick.y);
+		mMovementVector = Vector3.ProjectOnPlane((mCameraDirection * mMovementVector), Vector3.up).normalized * mWalkSpeed * Time.deltaTime;
+
+		// Applying movement
+		transform.forward = Vector3.Slerp(transform.forward, mMovementVector.normalized, mRotationSlerpParameter);
+		mMovementVector += new Vector3(0f, /*mVerticalMovement*/ 0f, 0f);
 		mCharacterController.Move(mMovementVector);
+	#endregion
+
+		// Debugging
+		VisualDebug();
 	}
 
 	private void FixedUpdate ()
 	{
+
 	}
+#endregion
 }
