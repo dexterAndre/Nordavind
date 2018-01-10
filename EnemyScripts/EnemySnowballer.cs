9039a3a42b88055af919b6f89440c1c992a8573b
Eirik Gjeruldsen <eirgje@gmail.com>
2018-01-10 10:05:37 +0100

    Snowballer pipeline complete
    The snowballer pipeline should now be working correctly.

--- EnemyScripts/EnemySnowballer.cs
+++ EnemyScripts/EnemySnowballer.cs
@@ -50,6 +50,12 @@ public class EnemySnowballer : EnemyController {
     /// </summary>
     [SerializeField]
     private float chargeForce = 25f;
+    
+    /// <summary>
+    /// This float should be tweaked, and is the distance from where this unit goes for a self destruction.
+    /// </summary>
+    [SerializeField]
+    private float explosionZoneDistanceToActive = 5f;
 
     /// <summary>
     /// Taking a charge timer, and subtracting 1 per second.
@@ -74,14 +80,52 @@ public class EnemySnowballer : EnemyController {
         if (charged)
         {
             float distance = (transform.position - mTargetToFollow.position).magnitude;
-            
+            if (distance < explosionZoneDistanceToActive) {
+                StartCoroutine(SelfDestructionStart());
+            }
         }
     }
     #endregion
 
     #region Explosion
 
+    /// <summary>
+    /// This bool is here to make sure no more iterations of the charge starts once you start self destruction.
+    /// </summary>
+    private bool selfDestructionInitialized = false;
 
+    /// <summary>
+    /// This float will be the duration this unit stands still before exploding.
+    /// <para>  This should depend on the difficulty to dodge, and the duration of the animation.</para>
+    /// </summary>
+    [SerializeField]
+    private float selfDestructionAnimationDuration = 1f;
+
+    /// <summary>
+    /// This will stop the velocity in the x- and y-axis, and after the ienumerator is complete it will initialize the explosion.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator SelfDestructionStart()
+    {
+        charged = false;
+        mRigidBody.velocity = new Vector3(0f, mRigidBody.velocity.y, 0f);
+        yield return new WaitForSeconds(selfDestructionAnimationDuration);
+        Explode();
+    }
+
+    /// <summary>
+    /// The function used to make this unit explode.
+    /// <para>  Will set the this gameobject to FALSE.</para>
+    /// <para>  Will spawn explosion particles.</para>
+    /// <para>  Will deal damage to hostile units inside the damage zone.</para>
+    /// </summary>
+    private void Explode()
+    {
+        print(this.gameObject.name + " just exploded");
+        //spawn deathParticle here.
+        this.gameObject.SetActive(false);
+    }
+        
 #endregion
 
     #region UpdateFunctions
@@ -97,7 +141,8 @@ public class EnemySnowballer : EnemyController {
             StartCoroutine(SearchForEnemy(2f));
         }
 
-        if (foundEnemy && !gettingReadyToCharge) {
+        if (foundEnemy && !gettingReadyToCharge)
+        {
             if(mNavMeshAgent.enabled == true)
                 Nav_SetNavMeshDestinationToMovingObject(mTargetToFollow);
             else
