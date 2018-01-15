f1b756c0af9adb7b9547c6151b43d1aa88608f38
Eirik Gjeruldsen <eirgje@gmail.com>
2018-01-15 12:35:02 +0100

    Revert "Merge branch 'master' into Enemies"
    This reverts commit f5cc986d399aa98aec324b9197471297340d1f3c, reversing
    changes made to b76a94355f16131c4598c296fefebda8964ba016.

--- EnemyScripts/EnemyController.cs
+++ EnemyScripts/EnemyController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-<<<<<<< HEAD
 
 public class EnemyController : Actor {
 
@@ -167,62 +166,11 @@ public class EnemyController : Actor {
     ///Saves the current position of this unit to mLastPosition.
     ///</summary>
     private void Nav_SaveCurrentPosition(Vector3 mPosition)
-=======
-public class EnemyController : MonoBehaviour {
-
-    #region protected-variables
-    protected NavMeshAgent mNavmeshAgent = null;
-    protected Animator mAnimator = null;
-    protected AudioSource mAudioSource = null;
-    protected float mSpeed = 10f;
-    protected float mDetectionRange = 10f;
-    protected Transform mTargetToFollow = null;
-    protected Vector3 mLastPosition = Vector3.zero;
-    protected bool followingMovingTarget = false;
-    #endregion
-
-
-    #region Pathfinding
-
-    protected void UpdateFollowingTarget() {
-        if (followingMovingTarget) {
-            mNavmeshAgent.SetDestination(mTargetToFollow.position);
-        }
-    }
-
-    protected void SetNavMeshDestinationToMovingObject(Transform target)
-    {
-        mTargetToFollow = target;
-        followingMovingTarget = true;
-    }
-
-    protected void SetNavMeshDestinationToCertainPosition(Vector3 newTargetPosition)
-    {
-        mNavmeshAgent.SetDestination(newTargetPosition);
-    }
-
-    protected void GoBackToIdleState()
-    {
-        mNavmeshAgent.SetDestination(mLastPosition);
-        followingMovingTarget = false;
-    }
-
-    protected void StopNavMesh()
-    {
-        mNavmeshAgent.enabled = false;
-    }
-    protected void StartNavMesh(){
-        mNavmeshAgent.enabled = true;
-    }
-
-    private void SaveCurrentPosition(Vector3 mPosition)
->>>>>>> master
     {
         mLastPosition = mPosition;
     }
     #endregion
 
-<<<<<<< HEAD
     #region Health / Game information
 
     /// <summary>
@@ -341,6 +289,4 @@ public class EnemyController : MonoBehaviour {
     }
 
     #endregion
-=======
->>>>>>> master
 }
